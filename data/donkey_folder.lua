acfdd8530d200859060202532f201da83d772adb
hong-ye <lirunde94@163.com>
2018-08-27 15:31:08 +0800

    Add files via upload

---
+++ data/donkey_folder.lua
@@ -0,0 +1,146 @@
+require 'image'
+paths.dofile('dataset.lua')
+opt.data = paths.concat(opt.DATA_ROOT, opt.phase)
+
+if not paths.dirp(opt.data) then
+    error('Did not find directory: ' .. opt.data)
+end
+
+local cache = "cache"
+local cache_prefix = opt.data:gsub('/', '_')
+os.execute('mkdir -p cache')
+local trainCache = paths.concat(cache, cache_prefix .. '_trainCache.t7')
+local input_nc = opt.input_nc 
+local output_nc = opt.output_nc
+local loadSize   = {input_nc, opt.loadSize}
+local sampleSize = {input_nc, opt.fineSize}
+
+
+local preprocessAandB = function(imA, imB)
+  imA = image.scale(imA, loadSize[2], loadSize[2])
+  imB = image.scale(imB, loadSize[2], loadSize[2])
+  local perm = torch.LongTensor{3, 2, 1}
+
+ imA = imA:index(1, perm)
+ imA = imA:mul(2):add(-1)
+  imB = imB:index(1, perm)
+  imB = imB:mul(2):add(-1)
+
+  assert(imA:max()<=1,"A: badly scaled inputs")
+  assert(imA:min()>=-1,"A: badly scaled inputs")
+  assert(imB:max()<=1,"B: badly scaled inputs")
+  assert(imB:min()>=-1,"B: badly scaled inputs")
+ 
+  
+  local oW = sampleSize[2]
+  local oH = sampleSize[2]
+  local iH = imA:size(2)
+  local iW = imA:size(3)
+  
+  if iH~=oH then     
+    h1 = math.ceil(torch.uniform(1e-2, iH-oH))
+  end
+  
+  if iW~=oW then
+    w1 = math.ceil(torch.uniform(1e-2, iW-oW))
+  end
+  if iH ~= oH or iW ~= oW then 
+    imA = image.crop(imA, w1, h1, w1 + oW, h1 + oH)
+    imB = image.crop(imB, w1, h1, w1 + oW, h1 + oH)
+  end
+  
+  if opt.flip == 1 and torch.uniform() > 0.5 then 
+    imA = image.hflip(imA)
+    imB = image.hflip(imB)
+  end
+  
+  return imA, imB
+end
+
+
+
+local function loadImageChannel(path)
+    local input = image.load(path, 3, 'float')
+    input = image.scale(input, loadSize[2], loadSize[2])
+    local oW = sampleSize[2]
+    local oH = sampleSize[2]
+    local iH = input:size(2)
+    local iW = input:size(3)
+    
+    if iH~=oH then     
+      h1 = math.ceil(torch.uniform(1e-2, iH-oH))
+    end
+    
+    if iW~=oW then
+      w1 = math.ceil(torch.uniform(1e-2, iW-oW))
+    end
+    if iH ~= oH or iW ~= oW then 
+      input = image.crop(input, w1, h1, w1 + oW, h1 + oH)
+    end
+    
+    
+    if opt.flip == 1 and torch.uniform() > 0.5 then 
+      input = image.hflip(input)
+    end
+    
+
+    local input_lab = image.rgb2lab(input)
+    local imA = input_lab[{{1}, {}, {} }]:div(50.0) - 1.0
+    local imB = input_lab[{{2,3},{},{}}]:div(110.0)
+    local imAB = torch.cat(imA, imB, 1)
+    assert(imAB:max()<=1,"A: badly scaled inputs")
+    assert(imAB:min()>=-1,"A: badly scaled inputs")
+    
+    return imAB
+end
+
+
+local function loadImage(path)
+   local input = image.load(path, 3, 'float')
+   local h = input:size(2)
+   local w = input:size(3)
+   local imA = image.crop(input, 0, 0, w/2, h)
+   local imB = image.crop(input, w/2, 0, w, h)
+   
+   return imA, imB
+end
+
+
+local mean,std
+local trainHook = function(self, path)
+   collectgarbage()
+   if opt.preprocess == 'regular' then
+     local imA, imB = loadImage(path)
+     imA, imB = preprocessAandB(imA, imB)
+     imAB = torch.cat(imA, imB, 1)
+   end
+   
+   if opt.preprocess == 'colorization' then 
+     imAB = loadImageChannel(path)
+   end
+
+   return imAB
+end
+
+--print('trainCache', trainCache)
+--print('Creating train metadata')
+--print('serial batch:, ', opt.serial_batches)
+trainLoader = dataLoader{
+    paths = {opt.data},
+    loadSize = {input_nc, loadSize[2], loadSize[2]},
+    sampleSize = {input_nc+output_nc, sampleSize[2], sampleSize[2]},
+    split = 100,
+    serial_batches = opt.serial_batches, 
+    verbose = true
+ }
+
+trainLoader.sampleHookTrain = trainHook
+
+collectgarbage()
+
+do
+   local class = trainLoader.imageClass
+   local nClasses = #trainLoader.classes
+   assert(class:max() <= nClasses, "class logic has error")
+   assert(class:min() >= 1, "class logic has error")
+end
